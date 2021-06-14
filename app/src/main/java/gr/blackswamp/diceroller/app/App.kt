1a429546b756218814d19a99b2e6743ff15817de
Yiannis Mavrelos <yiannis.vgr@gmail.com>
2021-06-14 23:30:07 +0300

    added crashlytics

--- app/src/main/java/gr/blackswamp/diceroller/app/App.kt
+++ app/src/main/java/gr/blackswamp/diceroller/app/App.kt
@@ -1,6 +1,9 @@
 package gr.blackswamp.diceroller.app
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.provider.Settings
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 import gr.blackswamp.diceroller.BuildConfig
 import gr.blackswamp.diceroller.R
@@ -15,5 +18,14 @@ class App @Inject constructor() : Application() {
         Logger.enable(debug = BuildConfig.DEBUG, appName = getString(R.string.app_name))
         Logger.setContext { applicationContext }
         //</editor-fold>
+        try {
+            @SuppressLint("HardwareIds")
+            val deviceId = Settings.Secure.getString(
+                applicationContext.contentResolver,
+                Settings.Secure.ANDROID_ID
+            ) ?: ""
+            FirebaseCrashlytics.getInstance().setCustomKey("DeviceId", deviceId)
+        } catch (ignored: Throwable) {
+        }
     }
 }
